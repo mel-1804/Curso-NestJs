bc5af8cdd932f49115b0c8243458f5e104423a0d
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2025-08-20 12:18:26 -0400

    se crea carpeta task con sus modules, services y controller

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,21 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class TasksService {
+  constructor(private prisma: PrismaService) {}
+
+  async getTasks() {
+    try {
+      const tasks = await this.prisma.task.findMany();
+      return tasks;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new HttpException(
+          error.message,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+}
