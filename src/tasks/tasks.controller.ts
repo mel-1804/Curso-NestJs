2b41500bd6921fa4168a55eba02e61b421cb48b5
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2025-08-22 19:47:22 -0400

    se crea 2 enpoints de tipo GET (mostrar todas las tareas y cada una por id), se valida uso de Pipes a nivel global en main.ts

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
+// ESTOS SON LOS ENDPOINTS----------------------------------------------------------------------
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksServices: TasksService) {}
-    @Get()
-    async getTasks(){
-        return this.tasksServices.getTasks();
-    }
+  constructor(private tasksServices: TasksService) {}
+  // MUESTRA TODAS LAS TAREAS
+  @Get() // en este árentesis puedo poner el path
+  async getTasks() {
+    return this.tasksServices.getTasks();
+  }
+
+  //MUESTRA UNA TAREA POR ID
+  @Get('/:id')
+  async getTask(@Param('id', ParseIntPipe) id: number) {
+    return await this.tasksServices.getTask(id);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 
 @Injectable()
@@ -15,6 +20,26 @@ export class TasksService {
           error.message,
           HttpStatus.INTERNAL_SERVER_ERROR,
         );
+      }
+      return [];
+    }
+  }
+
+  async getTask(id: number) {
+    try {
+      const task = await this.prisma.task.findFirst({
+        where: { id },
+      });
+      if (task) return task;
+      throw new NotFoundException(`Task with id ${id} not found`);
+    } catch (error: unknown) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(error.message);
+        if (error instanceof Error)
+          throw new HttpException(
+            error.message,
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
       }
       return [];
     }
